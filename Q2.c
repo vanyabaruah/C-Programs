d62f9245c425752377faf4f7fc9ffcbabf557818
vanyabaruah <vanya.jrs06@gmail.com>
2026-04-13 11:49:12 +0530

    Delete Q2.c

--- Q2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int c,f;
-    printf("enter the temperature in fahreneit");
-    scanf("%d",&f);
-    c=(f-32)*5/9;
-    printf("temperature in celcius %d",c);
-}
